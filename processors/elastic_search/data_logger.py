0434436c2f64f4a7a55a4367cdee2f91249d225b
Sakib Hasan <sakibccr@gmail.com>
2021-06-22 17:25:35 +0600

    added connection adapter

--- processors/elastic_search/data_logger.py
+++ processors/elastic_search/data_logger.py
@@ -6,6 +6,6 @@ def upload_log(data):
     es_host = config('ES_HOST')
     es_port = config('ES_PORT')
     method = 'POST'
-    url = f'{es_host}:{es_port}/activity-log-student/_doc/'
+    url = f'http://{es_host}:{es_port}/activity-log-student/_doc/'
     resp = requests.request(method, url, json=data)
     return resp.status_code
