4c40e3aaa58c5728e764537e70d75fa53d6c91b2
Sahidul Hassan <alrazon@gmail.com>
2021-06-28 20:08:41 +0600

    hubspot cart form update

--- processors/hubspot/data_service.py
+++ processors/hubspot/data_service.py
@@ -64,11 +64,6 @@ def send_product_data(data):
 
     url = f'https://api.hsforms.com/submissions/v3/integration/submit/{HUBSPOT_PORTAL_ID}/{HUBSPOT_CART_CREATION_FORM_ID}'
 
-    for field in data['fields']:
-        if field['name'] == 'cart_status' and field['value'].lower() == 'processed':
-            url = f'https://api.hsforms.com/submissions/v3/integration/submit/{HUBSPOT_PORTAL_ID}/{HUBSPOT_CART_UPDATE_FORM_ID}'
-            break
-
     resp = requests.post(url, json=data)
 
     return resp.status_code
