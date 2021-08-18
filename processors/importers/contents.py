b34439b9d22a3c879d35ed23b580d72ac82c9c56
Sakib Hasan <sakibccr@gmail.com>
2021-08-18 12:47:54 +0600

    instead of csv file, spreadsheets are being processed

--- processors/importers/contents.py
+++ processors/importers/contents.py
@@ -2,19 +2,23 @@ import json
 import pika
 from decouple import config
 from bson import ObjectId
-import csv
+from openpyxl import load_workbook
+from io import BytesIO
 import urllib.request
+import pandas as pd
 from loggers.mongo import save_status_to_mongo
 from config import mongo_client
 from django_initializer import initialize_django
 from datetime import datetime
 from django.utils.formats import get_format
+from itertools import groupby
 initialize_django()
 
 from shared_models.models import Course, Section
 from django_scopes import scopes_disabled
 from models.course.course import Course as CourseModel
 from models.course.section import Section as SectionModel
+from models.occupation.occupation import Occupation as OccupationModel
 
 
 def create_queue_postgres(import_task):
@@ -46,43 +50,74 @@ def import_courses_mongo(import_task):
     file_url = f'{remote_url}{filename}'
 
     response = urllib.request.urlopen(file_url)
-    lines = [line.decode('utf-8') for line in response.readlines()]
-    cr = csv.DictReader(lines)
-    items = list(cr)
-    print('got data from csv file')
+    resp_binary = response.read()
+    try:
+        df = pd.read_excel(resp_binary, sheet_name=import_task.import_type)
+    except ValueError:
+        print('could not retrieve data. possibly wrong file type.')
+        return
+
+    data = df.T.to_dict()
+    print('got data from spreadsheet file')
 
     try:
         mongo_client.connect_mongodb()
-        for data in items:
-            print('importing: ')
-            data = {k.strip(): v.strip() for (k, v) in data.items()}
-            data['image'] = {'original': data['image']}
-            data['default_image'] = {'original': data['default_image']}
-            data['from_importer'] = True
-            data['keywords'] = []
-            data['provider'] = ObjectId(import_task.course_provider.content_db_reference)
-            data['_is_deleted'] = False
+        is_success = True
+        for key, row in data.items():
+            row = {k.strip(): str(v).strip() for (k, v) in row.items()}
+            row['image'] = {'original': row['image']}
+            row['default_image'] = {'original': row['default_image']}
+            row['from_importer'] = True
+            row['keywords'] = []
+            row['provider'] = ObjectId(import_task.course_provider.content_db_reference)
+            row['_is_deleted'] = False
             try:
                 print('getting course document')
-                course_model = CourseModel.with_deleted_objects(external_id=data['external_id'], provider=data['provider'])
+                course_model = CourseModel.with_deleted_objects(external_id=row['external_id'], provider=row['provider'])
 
-                raw_query = {'$set': data}
+                raw_query = {'$set': row}
                 print('upserting course document')
                 course_model.update_one(__raw__=raw_query, upsert=True)
                 print('done')
-                import_task.status = 'completed'
-                import_task.queue_processed = 1
-                import_task.save()
-                create_queue_postgres(import_task)
+
             except Exception as e:
+                is_success = False
                 print('execption: ', str(e))
                 import_task.status = 'failed'
-                msg = {'message': str(e), 'import_task_id': str(import_task.id), 'external_id': data['external_id']}
+                msg = {'message': str(e), 'import_task_id': str(import_task.id), 'external_id': row['external_id']}
                 save_status_to_mongo(status_data=msg, collection='ImportTaskErrorLog')
-                import_task.status_message = data['external_id'] + ': create error'
+                import_task.status_message = row['external_id'] + ': create error'
                 import_task.save()
 
+            try:
+                df2 = pd.read_excel(resp_binary, sheet_name='careers')
+            except ValueError:
+                pass
+            else:
+                print('got career tagging data')
+                career_data = []
+
+                for key, row in df2.T.to_dict().items():
+                    career_data.append({k.strip(): str(v).strip() for (k, v) in row.items()})
+
+                for course_external_id, data in groupby(career_data, key=lambda x:x['course_external_id']):
+                    soc_codes = [item['soc_code'] for item in list(data)]
+                    print('updating course with career data: ', course_external_id, soc_codes)
+                    course_model = CourseModel.objects.get(external_id=course_external_id, provider=ObjectId(import_task.course_provider.content_db_reference))
+                    course_model.update(
+                        pull_all__careers=[item for item in OccupationModel.objects.all()]
+                    )
+                    course_model.update(add_to_set__careers=[career.id for career in OccupationModel.objects.filter(soc_code__in=soc_codes)])
+                    course_model.save()
+                    print('career tagging complete')
+
     finally:
+        if is_success:
+            import_task.status = 'completed'
+            import_task.queue_processed = 1
+            import_task.save()
+            create_queue_postgres(import_task)
+
         print('operation completed')
         mongo_client.disconnect_mongodb()
 
@@ -93,22 +128,16 @@ def import_courses_postgres(import_task):
     file_url = f'{remote_url}{filename}'
 
     response = urllib.request.urlopen(file_url)
-    lines = [line.decode('utf-8') for line in response.readlines()]
-    cr = csv.DictReader(lines)
-    items = list(cr)
+    df = pd.read_excel(response.read())
+    data = df.T.to_dict()
 
     try:
         mongo_client.connect_mongodb()
-        for data in items:
-            data = {k.strip(): v.strip() for (k, v) in data.items()}
-            data['image'] = {'original': data['image']}
-            data['default_image'] = {'original': data['default_image']}
-            data['from_importer'] = True
-            data['keywords'] = []
-            data['provider'] = ObjectId(import_task.course_provider.content_db_reference)
+        for key, row in data.items():
+            row = {k.strip(): str(v).strip() for (k, v) in row.items()}
 
             try:
-                course_model = CourseModel.objects.get(external_id=data['external_id'], provider=data['provider'])
+                course_model = CourseModel.objects.get(external_id=row['external_id'], provider=ObjectId(import_task.course_provider.content_db_reference))
             except CourseModel.DoesNotExist:
                 pass
             else:
@@ -127,7 +156,7 @@ def import_courses_postgres(import_task):
                                 external_image_url=course_model.default_image,
                             )
                         except Exception as e:
-                            msg = {'message': str(e), 'import_task_id': str(import_task.id), 'external_id': data['external_id']}
+                            msg = {'message': str(e), 'import_task_id': str(import_task.id), 'external_id': row['external_id']}
                             save_status_to_mongo(status_data=msg, collection='ImportTaskErrorLog')
                         import_task.queue_processed = 2
                     else:
@@ -152,71 +181,70 @@ def import_sections_mongo(import_task):
     file_url = f'{remote_url}{filename}'
 
     response = urllib.request.urlopen(file_url)
-    lines = [line.decode('utf-8') for line in response.readlines()]
-    cr = csv.DictReader(lines)
-    items = list(cr)
+    df = pd.read_excel(response.read())
+    data = df.T.to_dict()
 
     try:
         mongo_client.connect_mongodb()
-        for data in items:
-            data = {k.strip().replace('*', ''): v.strip() for (k, v) in data.items()}
+        for key, row in data.items():
+            row = {k.strip().replace('*', ''): str(v).strip() for (k, v) in row.items()}
 
-            data['course_fee'] = {'amount': data['course_fee'], 'currency': 'usd'}
-            data['instructors'] = []
+            row['course_fee'] = {'amount': row['course_fee'], 'currency': 'usd'}
+            row['instructors'] = []
             try:
-                int(data['num_seats'])
+                int(row['num_seats'])
             except ValueError:
-                data['num_seats'] = 0
+                row['num_seats'] = 0
 
             try:
-                int(data['available_seats'])
+                int(row['available_seats'])
             except ValueError:
-                data['available_seats'] = 0
+                row['available_seats'] = 0
 
             try:
-                float(data['credit_hours'])
+                float(row['credit_hours'])
             except ValueError:
-                data['credit_hours'] = 0.00
+                row['credit_hours'] = 0.00
 
             try:
-                float(data['ceu_hours'])
+                float(row['ceu_hours'])
             except ValueError:
-                data['ceu_hours'] = 0.00
+                row['ceu_hours'] = 0.00
 
             try:
-                float(data['clock_hours'])
+                float(row['clock_hours'])
             except ValueError:
-                data['clock_hours'] = 0.00
+                row['clock_hours'] = 0.00
 
-            if data['registration_deadline'] == '':
-                data['registration_deadline'] = datetime.now().date()
+            if row['registration_deadline'] == '':
+                row['registration_deadline'] = datetime.now().date()
 
             try:
-                float(data['load_hours'])
+                float(row['load_hours'])
             except ValueError:
-                data['load_hours'] = 0.00
+                row['load_hours'] = 0.00
 
-            if data['details_url'] == '':
-                data['details_url'] = None
+            if row['details_url'] == '':
+                row['details_url'] = None
 
-            data['execution_mode'] = str(data['execution_mode']).lower()
+            row['execution_mode'] = str(row['execution_mode']).lower()
 
-            data['provider'] = ObjectId(import_task.course_provider.content_db_reference)
+            row['provider'] = ObjectId(import_task.course_provider.content_db_reference)
             try:
-                course_model = CourseModel.with_deleted_objects.get(external_id=data.pop('course_external_id'), provider=data.pop('provider'))
+                course_model = CourseModel.with_deleted_objects.get(external_id=row.pop('course_external_id'), provider=row.pop('provider'))
             except CourseModel.DoesNotExist:
                 import_task.queue_processed = 2
                 import_task.status = 'failed'
             else:
 
-                if CourseModel.objects(id=course_model.id, sections__code=data['code']).count():
+                if CourseModel.objects(id=course_model.id, sections__code=row['code']).count():
                     print('exists.updating')
                     CourseModel.objects(
-                        id=course_model.id, sections__code=data['code']
-                    ).update_one(set__sections__S=SectionModel(**data))
+                        id=course_model.id, sections__code=row['code']
+                    ).update_one(set__sections__S=SectionModel(**row))
                 else:
                     print('does not exists. creating')
-                    course_model.sections.append(SectionModel(**data))
+                    course_model.sections.append(SectionModel(**row))
                     course_model.save()
 
                 import_task.queue_processed = 2
@@ -245,23 +273,22 @@ def import_sections_postgres(import_task):
     file_url = f'{remote_url}{filename}'
 
     response = urllib.request.urlopen(file_url)
-    lines = [line.decode('utf-8') for line in response.readlines()]
-    cr = csv.DictReader(lines)
-    items = list(cr)
+    df = pd.read_excel(response.read())
+    data = df.T.to_dict()
 
     try:
         mongo_client.connect_mongodb()
-        for data in items:
-            data = {k.strip().replace('*', ''): v.strip() for (k, v) in data.items()}
-            if data['available_seats'] == '':
-                data['available_seats'] = None
+        for key, row in data.items():
+            row = {k.strip().replace('*', ''): str(v).strip() for (k, v) in row.items()}
+            if row['available_seats'] == '':
+                row['available_seats'] = None
 
-            if data['num_seats'] == '':
-                data['num_seats'] = None
-            data['provider'] = ObjectId(import_task.course_provider.content_db_reference)
+            if row['num_seats'] == '':
+                row['num_seats'] = None
+            row['provider'] = ObjectId(import_task.course_provider.content_db_reference)
 
             try:
-                course_model = CourseModel.with_deleted_objects.get(external_id=data.pop('course_external_id'), provider=data.pop('provider'))
+                course_model = CourseModel.with_deleted_objects.get(external_id=row.pop('course_external_id'), provider=row.pop('provider'))
             except CourseModel.DoesNotExist:
                 pass
             else:
@@ -272,34 +299,34 @@ def import_sections_postgres(import_task):
                         pass
                     else:
                         try:
-                            section = Section.objects.get(course=course, name=data['code'])
+                            section = Section.objects.get(course=course, name=row['code'])
                         except Section.DoesNotExist:
                             Section.objects.create(
                                 course=course,
-                                name=data['code'],
-                                fee=data['course_fee'],
-                                seat_capacity=data['num_seats'],
-                                available_seat=data['available_seats'],
-                                execution_mode=data['execution_mode'],
+                                name=row['code'],
+                                fee=row['course_fee'],
+                                seat_capacity=row['num_seats'],
+                                available_seat=row['available_seats'],
+                                execution_mode=row['execution_mode'],
                                 registration_deadline=parse_date(data['registration_deadline']),
                                 content_db_reference=str(course_model.id),
                                 is_active=False,
-                                start_date=parse_date(data['start_date']),
-                                end_date=parse_date(data['end_date']),
-                                execution_site=data['execution_site'],
+                                start_date=parse_date(row['start_date']),
+                                end_date=parse_date(row['end_date']),
+                                execution_site=row['execution_site'],
                             )
                         else:
-                            section.name = data['code']
-                            section.fee = data['course_fee']
-                            section.seat_capacity = data['num_seats']
-                            section.available_seat = data['available_seats']
-                            section.execution_mode = data['execution_mode']
-                            section.registration_deadline = data['registration_deadline']
+                            section.name = row['code']
+                            section.fee = row['course_fee']
+                            section.seat_capacity = row['num_seats']
+                            section.available_seat = row['available_seats']
+                            section.execution_mode = row['execution_mode']
+                            section.registration_deadline = row['registration_deadline']
                             section.content_db_reference = str(course_model.id)
                             section.is_active = False
-                            section.start_date = data['start_date']
-                            section.end_date = data['end_date']
-                            section.execution_site = data['execution_site']
+                            section.start_date = row['start_date']
+                            section.end_date = row['end_date']
+                            section.execution_site = row['execution_site']
 
                         import_task.queue_processed = 2
 
