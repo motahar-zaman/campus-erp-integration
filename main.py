d1d5d4882af292a34d496009548270a22ad3f90e
Sahidul Hassan <alrazon@gmail.com>
2021-11-16 16:02:04 +0600

    code optimization

--- main.py
+++ main.py
@@ -10,27 +10,27 @@ def main():
     AMQP_PASS = config('AMQP_PASS')
     AMQP_HOST = config('AMQP_HOST')
     AMQP_PORT = config('AMQP_PORT')
-
     amqp_url = f'amqps://{AMQP_USER}:{AMQP_PASS}@{AMQP_HOST}:{AMQP_PORT}?connection_attempts=5&retry_delay=5'
+    exchange_campus = 'campusmq'
 
-    connection = pika.BlockingConnection(
-        pika.URLParameters(amqp_url))
-
+    connection = pika.BlockingConnection(pika.URLParameters(amqp_url))
     channel = connection.channel()
+    channel.exchange_declare(exchange=exchange_campus, exchange_type='topic')
 
-    channel.exchange_declare(exchange='campusmq', exchange_type='topic')
+    queue_enroll = 'mq_enroll'
+    channel.queue_declare(queue_enroll, exclusive=True)
+    channel.queue_bind(exchange=exchange_campus, queue=queue_enroll, routing_key='*.enroll')
+    channel.basic_consume(queue=queue_enroll, on_message_callback=enroll_callback, auto_ack=True)
 
-    retund_queue = channel.queue_declare('', exclusive=True)
-    channel.queue_bind(exchange='campusmq', queue=retund_queue.method.queue, routing_key='*.refund')
-    channel.basic_consume(queue=retund_queue.method.queue, on_message_callback=refund_callback, auto_ack=True)
+    queue_import = 'mq_import'
+    channel.queue_declare(queue_import, exclusive=True)
+    channel.queue_bind(exchange=exchange_campus, queue=queue_import, routing_key='*.import')
+    channel.basic_consume(queue=queue_import, on_message_callback=import_callback, auto_ack=True)
 
-    enroll_queue = channel.queue_declare('', exclusive=True)
-    channel.queue_bind(exchange='campusmq', queue=enroll_queue.method.queue, routing_key='*.enroll')
-    channel.basic_consume(queue=enroll_queue.method.queue, on_message_callback=enroll_callback, auto_ack=True)
-
-    import_queue = channel.queue_declare('', exclusive=True)
-    channel.queue_bind(exchange='campusmq', queue=import_queue.method.queue, routing_key='*.import')
-    channel.basic_consume(queue=import_queue.method.queue, on_message_callback=import_callback, auto_ack=True)
+    queue_refund = 'mq_refund'
+    channel.queue_declare(queue_refund, exclusive=True)
+    channel.queue_bind(exchange=exchange_campus, queue=queue_refund, routing_key='*.refund')
+    channel.basic_consume(queue=queue_refund, on_message_callback=refund_callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
