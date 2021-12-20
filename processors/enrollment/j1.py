c6d001a588a486af258647f4844f94253ce7a3e7
Zaman <zaman@sgcsoft.net>
2021-12-20 12:20:17 +0600

    enrollment-payment correction

--- processors/enrollment/j1.py
+++ processors/enrollment/j1.py
@@ -1,4 +1,11 @@
 from services.mindedge import MindEdgeService
+from campuslibs.loggers.mongo import save_to_mongo
+import requests
 
 def handle_j1_enrollment(data):
-    print(data)
+    save_to_mongo(data={'comment': 'input data', 'data': data})
+    url = 'http://PDSVC-UNITY.JENZABARCLOUD.COM:9090/ws/rest/campus/api/enrollment/create'
+
+    response = requests.post(url, params=data)
+    response_data = response.json()
+    save_to_mongo(data={'comment': 'response data', 'data': response_data}, collection='handle_j1_enrollment')
