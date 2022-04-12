6cd76346be39521c95dae11ae788118971b01db3
Zaman <zaman@sgcsoft.net>
2022-04-12 14:19:16 +0600

    [CP2-1904] Send Notification messages to related Partner

---
+++ processors/notification/notification.py
@@ -0,0 +1,91 @@
+from mongoengine import get_db, connect, disconnect
+from bson import ObjectId
+from decouple import config
+from django.utils import timezone
+from shared_models.models import Notification, NotificationLog, Event, Payment, Cart, EventSubscription, Partner
+from django_scopes import scopes_disabled
+
+
+def notification_to_course_provider(notification_id):
+    print('notification_to_course_provider')
+    try:
+        notification = Notification.objects.get(pk=notification_id)
+    except Notification.DoesNotExist:
+        print('notification DoesNotExist')
+        return False
+    else:
+        notification_type = notification.data['type']
+        ref_id = notification.data['id']
+
+        if notification_type == 'payment':
+            print('payment type notification')
+            with scopes_disabled():
+                try:
+                    payment = Payment.objects.get(pk=ref_id)
+                except Payment.DoesNotExist:
+                    print('payment DoesNotExist')
+                    return False
+                else:
+                    order = payment.cart
+        else:
+            print('order type notification')
+            with scopes_disabled():
+                try:
+                    order = Cart.objects.get(pk=ref_id)
+                except Cart.DoesNotExist:
+                    print('order DoesNotExist')
+                    return False
+
+        print("order id = " + str(order.id))
+        print(order.cart_items.all())
+
+        for item in order.cart_items.all():
+            print('in cart_items')
+            course_provider = item.product.store_course_section.store_course.course.course_provider
+            try:
+                partner = Partner.objects.get(ref_id=course_provider.id)
+            except Partner.DoesNotExist:
+                print('partner DoesNotExist')
+                return False
+
+            #check if partner is subscribed or not for the event
+            #if subscribed, send notification message
+            subscribed = None
+            try:
+                subscribed = EventSubscription.objects.get(partner=partner, event=notification.event)
+            except EventSubscription.DoesNotExist:
+                print('subscribed DoesNotExist')
+                pass
+
+            if subscribed:
+                print('subscribed')
+                url = partner.notification_submission_url
+                send_msg, response = send_message_to_course_provider(url, notification)
+                if send_msg:
+                    notification.status = Notification.STATUS_SUCCESSFUL
+                else:
+                    notification.status = Notification.STATUS_FAILED
+                notification.save()
+                print('notification status saved, log creating')
+
+                notification_log = NotificationLog.objects.create(
+                    notification=notification,
+                    partner=partner,
+                    log_time=timezone.now(),
+                    status=notification.status,
+                    http_response=response
+                )
+                print('notification log created')
+        print('notification processing complete')
+    return True
+
+
+def send_message_to_course_provider(url, data):
+    response = {'status': '200'}
+    return True, response
+
+    # headers = {
+    #     'Content-Type': 'application/json'
+    # }
+    # response = requests.request("POST", url, headers=headers, data=json.dumps(data))
+    # return response.json()
