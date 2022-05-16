118e8df774883740785eba7f8b33f7b6deb75a34
Zaman <zaman@sgcsoft.net>
2022-05-16 15:31:10 +0600

    [CP2-1952] HIR Publish API update implementation: Deleting new items

--- processors/publish/handle_data.py
+++ processors/publish/handle_data.py
@@ -13,7 +13,8 @@ from .helpers import (
     write_status,
     write_log,
     insert_into_mongo,
-    get_data
+    get_data,
+    es_course_unpublish
 )
 
 from .serializers import CourseSerializer, SectionSerializer, CourseModelSerializer,\
@@ -39,6 +40,8 @@ from django.utils import timezone
 from django_scopes import scopes_disabled
 from models.publish.publish_job import PublishJob as PublishJobModel
 from mongoengine import NotUniqueError
+from django.db import transaction
+
 
 def create_sections(doc, data, course_provider, course_provider_model, contracts=[]):
     # insert every item in mongo to get status individually
@@ -510,6 +513,289 @@ def create_products(doc, item, course_provider_model):
     return True
 
 
+def deactivate_course(doc, course_provider, course_provider_model, data):
+    # insert every item in mongo to get status individually
+    mongo_data = {'data': data, 'publish_job_id': doc['id'], 'type': 'course_delete', 'time': timezone.now(),
+                  'message': 'task is still in queue', 'status': 'pending',
+                  'external_id': data['match']['course']}
+
+    log_serializer = PublishLogModelSerializer(data=mongo_data)
+    if log_serializer.is_valid():
+        inserted_item = log_serializer.save()
+    else:
+        print(log_serializer.errors)
+
+    with scopes_disabled():
+        try:
+            course_model = CourseModel.objects.get(external_id = data['match']['course'], provider = course_provider.content_db_reference)
+        except CourseModel.DoesNotExist:
+            inserted_item.errors = {'course_model': ['course_model does not found in database']}
+            inserted_item.status = 'failed'
+            inserted_item.message = 'error occurred'
+            inserted_item.save()
+
+            return False
+
+        try:
+            course = Course.objects.get(content_db_reference=course_model.id, course_provider=course_provider)
+        except Course.DoesNotExist:
+            inserted_item.errors = {'course': ['course does not found in database']}
+            inserted_item.status = 'failed'
+            inserted_item.message = 'error occurred'
+            inserted_item.save()
+
+            return False
+
+    contracts = CourseSharingContract.objects.filter(course_provider=course_provider, is_active=True)
+
+    with scopes_disabled():
+        sections = course.sections.all()
+        store_courses = StoreCourse.objects.filter(course=course, store__in=[contract.store for contract in contracts])
+
+    # 4. Get store course sections
+    with scopes_disabled():
+        store_course_sections = StoreCourseSection.objects.filter(store_course__in=store_courses, section__in=sections)
+
+    #########################################################################################
+    # If all the sections of a course are not to be deleted, then the course is not touched.
+    # otherwise, the course is deactivated after deactivating all the sections.
+    # this works when no section is provide and we consider the whole course (e.g. all the section) for deactivation
+    # but will fail when all the sections are provide.
+    # in that case, we must manually check if the course has any section left active after the deactivation
+    # operation and if there's none, deactivate the course only then
+
+    # 1. Deactivate the Store Course Sections
+    with transaction.atomic():
+        with scopes_disabled():
+            store_course_sections.update(active_status=False)
+
+            # 2. Deactivate the Products associated with the store course_sections here
+            Product.objects.filter(
+                id__in=[scs.product.id for scs in store_course_sections]
+
+            ).update(active_status=False)
+
+            # 3. Now the Section
+            sections.update(active_status=False)
+
+            # 4. Now the Course if it has no active section
+            if course.sections.filter(active_status=True).count() == 0:
+                course.active_status = False
+                course.content_ready = False
+                course.save()
+                # if the course is deactivated, deactivate the store_courses too
+                store_courses.update(active_status=False, enrollment_ready=False)
+
+        inserted_item.message = 'task processed successfully'
+        inserted_item.status = 'completed'
+        inserted_item.save()
+
+    # once the store_course is deactivated, these must be then removed from ES too
+    for store_course in store_courses:
+        es_course_unpublish(store_course)
+
+
+def deactivate_section(doc, course_provider, course_provider_model, data):
+    # insert every item in mongo to get status individually
+    mongo_data = {'data': data, 'publish_job_id': doc['id'], 'type': 'section_delete', 'time': timezone.now(),
+                  'message': 'task is still in queue', 'status': 'pending',
+                  'external_id': data['match']['course']}
+
+    log_serializer = PublishLogModelSerializer(data=mongo_data)
+    if log_serializer.is_valid():
+        inserted_item = log_serializer.save()
+    else:
+        print(log_serializer.errors)
+
+    with scopes_disabled():
+        try:
+            course_model = CourseModel.objects.get(external_id = data['match']['course'], provider = course_provider.content_db_reference)
+        except CourseModel.DoesNotExist:
+            inserted_item.errors = {'course_model': ['course_model does not found in database']}
+            inserted_item.status = 'failed'
+            inserted_item.message = 'error occurred'
+            inserted_item.save()
+
+            return False
+
+        section_code = None
+        for section in course_model.sections:
+            if section.external_id == data['match']['section']:
+                section_code = section['code']
+                break
+
+        try:
+            course = Course.objects.get(content_db_reference=course_model.id, course_provider=course_provider)
+        except Course.DoesNotExist:
+            inserted_item.errors = {'course': ['course does not found in database']}
+            inserted_item.status = 'failed'
+            inserted_item.message = 'error occurred'
+            inserted_item.save()
+            return False
+
+        try:
+            section = course.sections.get(name=section_code)
+        except Section.DoesNotExist:
+            inserted_item.errors = {'course': ['section does not found in database']}
+            inserted_item.status = 'failed'
+            inserted_item.message = 'error occurred'
+            inserted_item.save()
+            return False
+        except Section.MultipleObjectsReturned:
+            inserted_item.errors = {'course': ['multiple section found in database']}
+            inserted_item.status = 'failed'
+            inserted_item.message = 'error occurred'
+            inserted_item.save()
+            return False
+
+    contracts = CourseSharingContract.objects.filter(course_provider=course_provider, is_active=True)
+
+    with scopes_disabled():
+        store_courses = StoreCourse.objects.filter(course=course, store__in=[contract.store for contract in contracts])
+
+    # 4. Get store course sections
+    with scopes_disabled():
+        store_course_sections = StoreCourseSection.objects.filter(store_course__in=store_courses, section=section)
+
+    #########################################################################################
+    # If all the sections of a course are not to be deleted, then the course is not touched.
+    # otherwise, the course is deactivated after deactivating all the sections.
+    # this works when no section is provide and we consider the whole course (e.g. all the section) for deactivation
+    # but will fail when all the sections are provide.
+    # in that case, we must manually check if the course has any section left active after the deactivation
+    # operation and if there's none, deactivate the course only then
+
+    # 1. Deactivate the Store Course Sections
+    with transaction.atomic():
+        with scopes_disabled():
+            store_course_sections.update(active_status=False)
+
+            # 2. Deactivate the Products associated with the store course_sections here
+            Product.objects.filter(
+                id__in=[scs.product.id for scs in store_course_sections]
+
+            ).update(active_status=False)
+
+            # 3. Now the Section
+            section.active_status=False
+            section.save()
+
+            # 4. Now the Course if it has no active section
+            if course.sections.filter(active_status=True).count() == 0:
+                course.active_status = False
+                course.content_ready = False
+                course.save()
+                # if the course is deactivated, deactivate the store_courses too
+                store_courses.update(active_status=False, enrollment_ready=False)
+
+        inserted_item.message = 'task processed successfully'
+        inserted_item.status = 'completed'
+        inserted_item.save()
+
+    # once the store_course is deactivated, these must be then removed from ES too
+    for store_course in store_courses:
+        es_course_unpublish(store_course)
+
+
+def deactivate_schedule(doc, course_provider, course_provider_model, data):
+    # insert every item in mongo to get status individually
+    mongo_data = {'data': data, 'publish_job_id': doc['id'], 'type': 'schedule_delete', 'time': timezone.now(),
+                  'message': 'task is still in queue', 'status': 'pending',
+                  'external_id': data['match']['course']}
+
+    log_serializer = PublishLogModelSerializer(data=mongo_data)
+    if log_serializer.is_valid():
+        inserted_item = log_serializer.save()
+    else:
+        print(log_serializer.errors)
+
+    try:
+        course_model = CourseModel.objects.get(external_id = data['match']['course'], provider = course_provider.content_db_reference)
+    except CourseModel.DoesNotExist:
+        inserted_item.errors = {'course_model': ['course_model does not found in database']}
+        inserted_item.status = 'failed'
+        inserted_item.message = 'error occurred'
+        inserted_item.save()
+        return False
+
+    for section_idx, section in enumerate(course_model.sections):
+        if section.external_id == data['match']['section']:
+            schedules = section.schedules
+            for idx, schedule in enumerate(section.schedules):
+                if schedule.external_id == data['match']['external_id']:
+                    schedules.pop(idx)
+                    course_model.sections[section_idx].schedules = schedules
+                    course_model.save()
+
+                    inserted_item.message = 'task processed successfully'
+                    inserted_item.status = 'completed'
+                    inserted_item.save()
+                    return True
+
+    inserted_item.errors = {'course_model': ['matching schedule does not found in database']}
+    inserted_item.status = 'failed'
+    inserted_item.message = 'error occurred'
+    inserted_item.save()
+    return False
+
+
+def deactivate_instructor(doc, course_provider, course_provider_model, data):
+    # insert every item in mongo to get status individually
+    mongo_data = {'data': data, 'publish_job_id': doc['id'], 'type': 'instructor_delete', 'time': timezone.now(),
+                  'message': 'task is still in queue', 'status': 'pending',
+                  'external_id': data['match']['course']}
+
+    log_serializer = PublishLogModelSerializer(data=mongo_data)
+    if log_serializer.is_valid():
+        inserted_item = log_serializer.save()
+    else:
+        print(log_serializer.errors)
+
+    try:
+        course_model = CourseModel.objects.get(external_id = data['match']['course'], provider = course_provider.content_db_reference)
+    except CourseModel.DoesNotExist:
+        inserted_item.errors = {'course_model': ['course_model does not found in database']}
+        inserted_item.status = 'failed'
+        inserted_item.message = 'error occurred'
+        inserted_item.save()
+        return False
+
+    try:
+        instructor_model = InstructorModel.objects.get(external_id=data['match']['external_id'], provider=course_provider.content_db_reference)
+    except InstructorModel.DoesNotExist:
+        inserted_item.errors = {'course_model': ['instructor does not found in database']}
+        inserted_item.status = 'failed'
+        inserted_item.message = 'error occurred'
+        inserted_item.save()
+        return False
+    except InstructorModel.MultipleObjectsReturned:
+        inserted_item.errors = {'course_model': ['multiple instructor exists in database']}
+        inserted_item.status = 'failed'
+        inserted_item.message = 'error occurred'
+        inserted_item.save()
+        return False
+
+    for section_idx, section in enumerate(course_model.sections):
+        if section.external_id == data['match']['section']:
+            instructors = section.instructors
+            for idx, instructor in enumerate(section.instructors):
+                if instructor == instructor_model:
+                    instructors.pop(idx)
+                    course_model.sections[section_idx].instructors = instructors
+                    course_model.save()
+
+                    inserted_item.message = 'task processed successfully'
+                    inserted_item.status = 'completed'
+                    inserted_item.save()
+                    return True
+
+    inserted_item.errors = {'course_model': ['matching instructor does not found in database']}
+    inserted_item.status = 'failed'
+    inserted_item.message = 'error occurred'
+    inserted_item.save()
+    return False
+
+
 def publish(doc_id):
     doc = PublishJobModel.objects.get(id=doc_id)
 
@@ -581,18 +867,18 @@ def publish(doc_id):
                     create_products(doc, item, course_provider_model)
 
         elif action == "record_delete":
-            create_courses(doc, course_provider, course_provider_model, records, contracts=contracts)
-
             for item in records:
-                if item['type'] == 'section':
-                    create_sections(doc, item, course_provider, course_provider_model, contracts=contracts)
+                if item['type'] == 'course':
+                    deactivate_course(doc, course_provider, course_provider_model, item)
 
-            for item in records:
-                if item['type'] == 'schedule':
-                    create_schedules(doc, item, course_provider_model)
+                elif item['type'] == 'section':
+                    deactivate_section(doc, course_provider, course_provider_model, item)
+
+                elif item['type'] == 'schedule':
+                    deactivate_schedule(doc, course_provider, course_provider_model, item)
+
                 elif item['type'] == 'instructor':
-                    create_instructors(doc, item, course_provider_model)
-                elif item['type'] == 'product':
-                    create_products(doc, item, course_provider_model)
+                    deactivate_instructor(doc, course_provider, course_provider_model, item)
+
 
         print('message processing complete')
